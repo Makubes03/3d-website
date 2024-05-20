fa4baedcedd5892ba1f140824afa44bc73478873
Makubes03 <2472529620@qq.com>
2024-05-20 20:57:09 +0800

    Add files via upload

---
+++ src/resources/textures.js
@@ -0,0 +1,62 @@
+let billboardTextures = {};
+// 第一块展板图片地址
+billboardTextures.blogTexture = '../src/jsm/csdn.png';
+// 第二块展板图片地址
+billboardTextures.musicTexture =
+  '../src/jsm/githubpro.png';
+// 第三块展板图片地址
+//billboardTextures.fundTexture ='../src/jsm/fund.png';
+
+let boxTexture = {};
+// 链接的图标地址
+boxTexture.Github = 'src/jsm/githubLogo.png';
+boxTexture.BiliBili = 'src/jsm/BiliBili.png';
+boxTexture.QQ = 'src/jsm/qq.png';
+boxTexture.mail = 'src/jsm/envelope.png';
+boxTexture.reactIcon = 'src/jsm/react.png';
+boxTexture.allSkills = 'src/jsm/OIP-C.png';
+boxTexture.lensFlareMain = 'src/jsm/lensflare0.png';
+//boxTexture.skrillex = 'src/jsm/skrillex.png';
+//boxTexture.edmText = 'src/jsm/EDM.png';
+
+// 砖块材质
+let stoneTexture = '../src/jsm/stone.png';
+// 展板木头材质
+let woodTexture = '../src/jsm/woodTexture.jpg';
+
+// 文字图片地址
+let inputText = {};
+//inputText.terpSolutionsText = '../src/jsm/terp-solutions-text.png';
+inputText.activities = '../src/jsm/aim.png';
+//inputText.bagholderBetsText = '../src/jsm/bagholderbets-text.png';
+inputText.homeSweetHomeText = '../src/jsm/home-sweet-home-text.png';
+//inputText.staticPortfolio = '../src/jsm/static-portfolio.png';
+inputText.pcControl = '../src/jsm/pc-control.png'
+inputText.mobileControl = '../src/jsm/mobile-control.png'
+inputText.link = '../src/jsm/link.png'
+
+//SVG
+let SVG = {};
+SVG.reactLogo = '../src/jsm/react-svg.svg';
+
+// 链接的跳转地址
+let URL = {};
+URL.blog =
+  'https://blog.csdn.net/weixin_63500345?type=blog';
+URL.ryanfloyd = 'https://huabyte.com';
+//URL.fund = 'https://github.com/AirHua-byte/FundSpider';
+URL.gitHub = 'https://github.com/Makubes03';
+URL.BiliBili = 'https://space.bilibili.com/316782653';
+URL.email = 'https://2472529620@qq.com';
+URL.music = 'https://makubes03.github.io/';
+URL.devTo =
+  'http://wpa.qq.com/msgrd?v=3&uin=3301833942&site=qq&menu=yes"><img border="0" src="http://wpa.qq.com/pa?p=2:3301833942:41';
+
+export {
+  billboardTextures,
+  boxTexture,
+  inputText,
+  URL,
+  stoneTexture,
+  woodTexture,
+};
